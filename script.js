b561772a91baa484b7f12d30645992bf45cdbaf8
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-03 21:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const text=document.getElementById("text");
+const delay=document.getElementById("delay");
+const submit=document.getElementById("btn");
+const output=document.getElementById("output");
+
+submit.addEventListener("click", async ()=>{
+	setTimeout(()=>{
+		output.textContent=`${text.value}`
+		text.value=""
+		delay.value=""
+	},delay.value)
+})
+
+
